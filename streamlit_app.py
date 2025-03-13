3dcc47b8314960b235ef6b1836dfb770fcf1797f
vedclove <vishal.lavangare@outlook.com>
2025-03-12 22:24:48 -0400

    solved use_column_width warning

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ image = None  # Initialize image variable
 # If user uploads a file
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
 # If user enters a URL
 elif image_url:
